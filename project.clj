e794da10c1bc05058b549cfaccb7d59cbcfb48d0
Seth Baxter <sethbaxter@prominentedge.com>
2015-12-24 09:27:35 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject refapp-devops "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main ^:skip-aot refapp-devops.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
