0cad954ea05f46bc0d48b8eae07165475b01698b
Seth Baxter <sethbaxter@prominentedge.com>
2015-12-24 09:39:39 -0500

    Deps

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject refapp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+(defproject refapp "0.1.0"
+  :description "Just a simple hello world."
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]]
   :main ^:skip-aot refapp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
