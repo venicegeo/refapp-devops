0879453da83c07015f10d468fef03231acfab5f1
Seth Baxter <sethbaxter@prominentedge.com>
2015-12-30 13:16:26 -0500

    Revert to scripts directory; add health-check script

---
+++ scripts/health-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+pushd `dirname $0` > /dev/null
+base=$(pwd -P)
+popd > /dev/null
+
+# gather some data about the repo
+source $base/vars.sh
+
+[ `curl -s -o /dev/null -w "%{http_code}" http://refapp-devops.cf.piazzageo.io` = 200 ]
