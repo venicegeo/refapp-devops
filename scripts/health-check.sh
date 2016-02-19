21f9ab52d73105287b54bb1bc64465f2d99aef9c
Seth Baxter <sethbaxter@prominentedge.com>
2016-02-19 13:26:16 -0500

    Fix health-check

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -7,4 +7,4 @@ popd > /dev/null
 # gather some data about the repo
 source $base/vars.sh
 
-[ `curl -s -o /dev/null -w "%{http_code}" http://$APP-$SHA.apps.cf2.piazzageo.io` = 200 ]
+[ `curl -s -o /dev/null -w "%{http_code}" http://$APP-$SHA.$DOMAIN` = 200 ]
